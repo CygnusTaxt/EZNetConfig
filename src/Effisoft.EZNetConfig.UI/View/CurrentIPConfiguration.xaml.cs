639d611f4ce4a81270d3da86c7f438aa9048064c
Jorge Aguilar <jorgea.aguilarh@gmail.com>
2014-10-18 23:20:09 -0500

    Added View OBject

---
+++ src/Effisoft.EZNetConfig.UI/View/CurrentIPConfiguration.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Effisoft.EZNetConfig.UI.View
+{
+    /// <summary>
+    /// Description for CurrentIPConfiguration.
+    /// </summary>
+    public partial class CurrentIPConfiguration : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the CurrentIPConfiguration class.
+        /// </summary>
+        public CurrentIPConfiguration()
+        {
+            InitializeComponent();
+        }
+    }
+}
